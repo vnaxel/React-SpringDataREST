f99406c8bc73faf1b3404ae1d01c918e1aee723d
Axel Yvenou <axel.yvenou@gmail.com>
2021-10-02 20:11:07 +0200

    created createDialog component + styles

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,6 +2,10 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+const follow = require('./follow');
+
+const root = '/api';
+
 class App extends React.Component { 
 
 	constructor(props) {
@@ -10,9 +14,27 @@ class App extends React.Component {
 	}
 
 	componentDidMount() { 
-		client({method: 'GET', path: '/api/employees'}).done(res => {
-			this.setState({employees: res.entity._embedded.employees});
-		});
+		this.loadFromServer(this.state.pageSize);
+	}
+	
+	loadFromServer(pageSize) {
+		follow(client, root, [
+			{rel: 'employees', params: {size: pageSize}}
+		]).then(employeeCollection => client({
+            method: 'GET',
+            path: employeeCollection.entity._links.profile.href,
+            headers: { 'Accept': 'application/schema+json' }
+        }).then(schema => {
+            this.schema = schema.entity;
+            return employeeCollection;
+        })).done(employeeCollection => {
+			this.setState({
+				employees: employeeCollection.entity._embedded.employees,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: pageSize,
+				links: employeeCollection.entity._links
+			});
+		})
 	}
 
 	render() { 
@@ -53,6 +75,53 @@ class Employee extends React.Component{
 		)
 	}
 }
+
+class CreateDialog extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		const newEmployee = {};
+		this.props.attributes.forEach(attribute => {
+			newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onCreate(newEmployee);
+		
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+		});
+		
+		window.location = "#";
+	}
+	
+	render() {
+		const inputs = this.props.attributes.map(attribute =>
+			<p key={attribute}>
+				<input type="text" placeholder={attribute} ref={attribute} className="field"/>
+			</p>
+		);
+		
+		return (
+			<div>
+				<a href="#createEmployee">Create</a>
+				<div id="createEmployee" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+							<h2>Create new employee</h2>
+							<form>
+								{inputs}
+								<button onClick={this.handleSubmit}>Create</button>
+							</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+} 
 
 ReactDOM.render(
 	<App />,
