2eb5a18e25ead3cffa99b210dfe11184d68c3cd0
Axel Yvenou <axel.yvenou@gmail.com>
2021-10-02 17:20:42 +0200

    refactor client for api calls

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const path = require('path');
 
-export const entry = './src/main/js/app.js';
-export const devtool = 'sourcemaps';
-export const cache = true;
-export const mode = 'development';
-export const output = {
-    path: __dirname,
-    filename: './src/main/resources/static/built/bundle.js'
-};
-export const module = {
-    rules: [
-        {
-            test: join(__dirname, '.'),
-            exclude: /(node_modules)/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"]
-                }
-            }]
-        }
-    ]
+module.exports = {
+    entry: './src/main/js/app.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    }
 };
